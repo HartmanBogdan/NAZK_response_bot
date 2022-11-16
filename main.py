9c94727d1ff7c81b5158f2ea8518ea1da26d3297
HartmanBogdan <bodyas584542>
2022-11-16 16:30:03 +0200

    up

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import csv
 # in TELEGRAM - https://t.me/check_response_bot
 
 bot = telebot.TeleBot("5521564357:AAEyEl6zrML9PGMY-vunQesbEU0d4ZWdw1Y", parse_mode=None)
-#to run script when u close ssh session - use tmax on linux terminal
-#https://askubuntu.com/questions/8653/how-to-keep-processes-running-after-ending-ssh-session
+# to run script when u close ssh session - use tmax on linux terminal
+# https://askubuntu.com/questions/8653/how-to-keep-processes-running-after-ending-ssh-session
 # in TELEGRAM - https://t.me/nazk_up_bot
 sitepack = []
 sitepack_plus = []
@@ -37,15 +37,16 @@ start - (старт/стоп) зупини, перед тим, як відпра
 help - вимоги до файлу, та підсказки.
 clean - очищення ран-тайм списку.
 check_message_urls - перевірити ран-тайм список.
-start_nazk - перевірити ресурки НАЗК. таймаут - 1хв
-stop_nazk - зупинити перевірку ресурсів НАЗК.
+up_nazk - перевірити ресурки НАЗК. таймаут - 1хв
+nazk_stop - зупинити перевірку ресурсів НАЗК.
 """
+
 
 def time_func():
     return (datetime.datetime.now()).strftime("%d:%m:%y %H:%M:%S")
 
 
-with open('logout.csv', 'a+', newline='',encoding='utf-8') as csvfile:
+with open('logout.csv', 'a+', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['Назва операції', 'Результат', 'Час виконання: ДД/ММ/РР Год/Хв/Сек']
     logger = csv.DictWriter(csvfile, fieldnames=fieldnames)
     logger.writeheader()
@@ -58,22 +59,26 @@ def send_welcome(message):
     global sitepack
     global WhileLoopFlag
     sitepack = []
-    bot.reply_to(message, "Привіт, тут є два режими як працює цей бот:\n\n"
+    bot.reply_to(message, "Привіт, тут є три режими як працює цей бот:\n\n"
                           "1)Скинь файл, що називається 'urls.txt', в якому є список посилань(більше 1 посилання) "
                           "розділених між собою переносом строки(ентер на клавіатурі) і бот автоматично буде "
-                          "перевіряти всі посилання. \n Щоб зупинити бота - пропиши: "
-                          " '/start'.\n\n"
-                          "Хочеш скинути новий список в файлі? Спочатку пропиши '/start'"
+                          "перевіряти всі посилання. \nЩоб зупинити бота - пропиши: "
+                          " /start.\n\n"
+                          "Хочеш скинути новий список в файлі? Спочатку пропиши /start"
                           "\n"
                           "\n"
                           "2)Кидай посилання в чат, бот буде записувати їх в список (дублікати ігноруються) та відправ "
                           "+ в чат, щоб почати перевірку цього (окремого)списку. Щоб обнулити список - пропиши "
-                          "команду '/clean' \n\n "
+                          "команду /clean \n\n"
                           "Режими незалежні один від одного та можуть працювати паралельно.\n\n"
-                          "!!!Вимоги до автоматичної перевірки - /help !!!")
+                          "!!!Вимоги до автоматичної перевірки - /help !!!"
+                          "\n"
+                          "\n"
+                            "3) Перевірка основних сервісів НАЗК. Почати - /up_nazk . Зупинити -  /nazk_stop")
 
     WhileLoopFlag = False
     logger_writer(first_par="send_welcome(message):Запуск чи перезапуск бота(не скрипта)", sec_par=" LOADED")
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -86,8 +91,9 @@ def help(message):
                      "5) Щоб почати перевірку нового файлу - потрібно спочатку зупинити бота /start ,"
                      "а потім відправити новий файл \"urls.txt\" ")
 
+
 def logger_writer(first_par, sec_par):
-    with open('logout.csv', 'a+', newline='',encoding='utf-8') as csvfile:
+    with open('logout.csv', 'a+', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['Назва операції', 'Результат', 'Час виконання: ДД/ММ/РР Год/Хв/Сек']
         logger = csv.DictWriter(csvfile, fieldnames=fieldnames)
         logger.writerow({'Назва операції': first_par, 'Результат': sec_par,
@@ -157,8 +163,7 @@ def check_message_urls(message):
     global sitepack_plus
     loaded = 0
     failed = 0
-    #if message.text == "+" and sitepack_plus is not None:
-
+    # if message.text == "+" and sitepack_plus is not None:
 
     for x in sitepack_plus:
 
@@ -180,22 +185,6 @@ def check_message_urls(message):
     bot.send_message(message.chat.id, str(loaded) + "/" + str(loaded + failed) + " - LOADED")
     logger_writer(first_par='Перевірка з списку відправлених в чаті посилань.',
                   sec_par=str(loaded) + "/" + str(loaded + failed) + " - LOADED ")
-
-
-@bot.message_handler(content_types=['text'])
-def add_to_sitepack(message):
-    global sitepack_plus
-    if message.text.startswith("http") and message.text not in sitepack_plus:
-        sitepack_plus.append(message.text)
-        logger_writer(first_par='В збережені посилання додано: ',
-                      sec_par=message.text)
-        print("Збережені посилання:" + str(sitepack_plus))
-    try:
-        sitepack_plus[0]
-    except IndexError:
-        bot.send_message(message.chat.id, "Повідомлення не містить посилання на сайт")
-        logger_writer(first_par='Перевірка з списку відправлених в чаті посилань.',
-                      sec_par=message.text + "  -  Повідомлення не містить посилання на сайт")
 
 
 def ssl_check(hostname):
@@ -238,8 +227,8 @@ def ssl_alarm_check(message, hostname):
                       sec_par=hostname + " ---- до закінчення SSL - сертифікату: " + str(ssl_warning) + " днів")
 
 
-@bot.message_handler(commands=['start_nazk'])
-def start_nazk(message):
+@bot.message_handler(commands=['up_nazk'])
+def up_nazk(message):
     global sitepack_nazk
     global WhileLoopFlag_nazk
     WhileLoopFlag_nazk = True
@@ -270,14 +259,14 @@ def start_nazk(message):
             logger_writer(first_par="Автоматична перевірка ресурсів НАЗК:   ",
                           sec_par=str_result)
 
-            #bot.send_message(message.chat.id, str(loaded) + "/" + str(sitepack_nazk.__len__()) + " - LOADED ")
+            # bot.send_message(message.chat.id, str(loaded) + "/" + str(sitepack_nazk.__len__()) + " - LOADED ")
             if loaded == 0:
-                bot.send_message(message.chat.id,"Check bot serverside connection or all urls are failed")
+                bot.send_message(message.chat.id, "Check bot serverside connection or all urls are failed")
             print(sitepack_nazk.__len__())
 
         except TypeError:
             bot.send_message(message.chat.id,
-                             "TypeError exeption в методі start_nazk(), ")
+                             "TypeError exeption в методі up_nazk(), ")
         time.sleep(60)
 
 
@@ -307,8 +296,8 @@ def ssl_check_nazk(hostname):
         return x
 
 
-@bot.message_handler(commands=['stop_nazk'])
-def stop_nazk(message):
+@bot.message_handler(commands=['nazk_stop'])
+def nazk_stop(message):
     global WhileLoopFlag_nazk
     WhileLoopFlag_nazk = False
     bot.send_message(message.chat.id, 'Роботу бота по перевірці ресурсів НАЗК зупинено')
@@ -316,11 +305,27 @@ def stop_nazk(message):
                   sec_par='Роботу бота по перевірці ресурсів НАЗК зупинено')
 
 
+@bot.message_handler(content_types=['text'])
+def add_to_sitepack(message):
+    global sitepack_plus
+    if message.text.startswith("http") and message.text not in sitepack_plus:
+        sitepack_plus.append(message.text)
+        logger_writer(first_par='В збережені посилання додано: ',
+                      sec_par=message.text)
+        print("Збережені посилання:" + str(sitepack_plus))
+    try:
+        sitepack_plus[0]
+    except IndexError:
+        bot.send_message(message.chat.id, "Повідомлення не містить посилання на сайт")
+        logger_writer(first_par='Перевірка з списку відправлених в чаті посилань.',
+                      sec_par=message.text + "  -  Повідомлення не містить посилання на сайт")
+
+
 while True:
     try:
         bot.polling(non_stop=True, interval=0)
     except Exception as e:
         print(e)
-        logger_writer(first_par= e, sec_par= "     ПОМИЛКА ПІДКЛЮЧЕННЯ")
+        logger_writer(first_par=e, sec_par="     ПОМИЛКА ПІДКЛЮЧЕННЯ")
         time.sleep(5)
         continue
